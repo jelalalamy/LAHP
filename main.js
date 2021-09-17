7239433b86da0f597ed52839ca4b922aa13a34d7
Jeremy La <jeremylai511@gmail.com>
2021-09-17 00:13:51 -0400

    Implemented Weather

--- main.js
+++ main.js
@@ -1,17 +1,64 @@
+const api = {
+    key: "dbd0c6b68f6d0c9d65497957302968db",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
 
 
-function main(){
-    showTime();
-    setInterval(showTime, 1000);
+showTime();
+getResults("Toronto");
+setInterval(() => getResults("Toronto"), 600000);
+setInterval(showTime, 1000);
 
-    document.getElementById("bgButtons").addEventListener('click', (e) => {
-        if (e.target.tagName !== 'BUTTON') return;
-        targetId = e.target.id;
-        selectedBackground = targetId.slice(6);
-        console.log("Selected background: " + selectedBackground);
-        document.getElementById("pageBody").className = selectedBackground;
+document.getElementById("bgButtons").addEventListener('click', (e) => {
+    if (e.target.tagName !== 'BUTTON') return;
+    targetId = e.target.id;
+    selectedBackground = targetId.slice(6);
+    console.log("Selected background: " + selectedBackground);
+    document.getElementById("pageBody").className = selectedBackground;
 
-    });
+});
+
+function getResults (location) {
+    fetch(`${api.base}weather?q=${location}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    /*
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+    */
+}
+
+function dateBuilder (d) { 
+    let months = ["January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+                "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${month} ${date} ${year}`;
 }
 
 function showTime(){
@@ -43,4 +90,3 @@ function showTime(){
 
 }
 
-main();
